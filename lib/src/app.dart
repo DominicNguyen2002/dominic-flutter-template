ad9dff6c9170c0eb5485a50df685d027af7c6616
Dominic-Nguyen <dominic.nguyen.hoang@gmail.com>
2024-04-09 12:00:45 +0700

    setup: add i18n

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,20 +1,20 @@
+import 'package:dominic_flutter_template/src/localization/localization_utils.dart';
 import 'package:dominic_flutter_template/src/themes/screen.dart';
 import 'package:dominic_flutter_template/src/themes/themes.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     AppScreens.mediaQuery = MediaQuery.of(context);
 
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: S.localizationsDelegates,
+      supportedLocales: S.supportedLocales,
+      onGenerateTitle: (BuildContext context) => S.of(context).common_appTitle,
       theme: AppTheme.light(),
       darkTheme: AppTheme.dark(),
     );
