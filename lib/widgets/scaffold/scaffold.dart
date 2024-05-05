2f3e61266f7208e762042a4cb6fa00ce916298b0
Dominic-Nguyen <dominic.nguyen.hoang@gmail.com>
2024-05-05 22:27:16 +0700

    feat: responsive with landscape and portrait

--- lib/widgets/scaffold/scaffold.dart
+++ lib/widgets/scaffold/scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:dominic_flutter_template/src/features/settings/logic/settings_bloc.dart';
+import 'package:dominic_flutter_template/src/utils/responsive_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,11 +7,23 @@ class XScaffold extends StatelessWidget {
   const XScaffold({
     super.key,
     this.appBar,
-    required this.child,
+    this.landscapeSmallScreen = const SizedBox(),
+    this.landscapeMediumScreen = const SizedBox(),
+    this.landscapeLargeScreen = const SizedBox(),
+    this.portraitSmallScreen = const SizedBox(),
+    this.portraitMediumScreen = const SizedBox(),
+    this.portraitLargeScreen = const SizedBox(),
+    this.child,
   });
 
   final PreferredSizeWidget? appBar;
-  final Widget child;
+  final Widget? child;
+  final Widget landscapeSmallScreen;
+  final Widget landscapeMediumScreen;
+  final Widget landscapeLargeScreen;
+  final Widget portraitSmallScreen;
+  final Widget portraitMediumScreen;
+  final Widget portraitLargeScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +33,43 @@ class XScaffold extends StatelessWidget {
         builder: (context, state) {
           return state.safeArea
               ? SafeArea(
-                  child: child,
+                  child: _buildWidget(),
                 )
-              : child;
+              : _buildWidget();
         },
       ),
     );
+  }
+
+  Widget _buildWidget() {
+    if (child != null) {
+      return child!;
+    } else if (ResponsiveUtils.isLandscapeScreen()) {
+      return _buildScreen(
+        smallScreen: landscapeSmallScreen,
+        mediumScreen: landscapeMediumScreen,
+        largeScreen: landscapeLargeScreen,
+      );
+    } else {
+      return _buildScreen(
+        smallScreen: portraitSmallScreen,
+        mediumScreen: portraitMediumScreen,
+        largeScreen: portraitLargeScreen,
+      );
+    }
+  }
+
+  Widget _buildScreen({
+    required Widget smallScreen,
+    required Widget mediumScreen,
+    required Widget largeScreen,
+  }) {
+    if (ResponsiveUtils.isSmallScreen()) {
+      return smallScreen;
+    } else if (ResponsiveUtils.isMediumScreen()) {
+      return mediumScreen;
+    } else {
+      return largeScreen;
+    }
   }
 }
